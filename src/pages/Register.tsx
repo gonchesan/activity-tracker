8783e5413b89711755f0c0521506179c101b8045
Gonchesan <gmsanchezgaray@gmail.com>
2024-01-30 22:31:50 -0300

    Add (App):Register Page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,117 @@
+import useAuth from '@/hooks/useAuth';
+import useForm from '@/hooks/useForm';
+import { SignInType } from '@/interface/auth';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const RegisterPage: React.FC = () => {
+    const { signUpNewUser, signInWithGoogle } = useAuth();
+    const { formData, handleInputChange, getValues, setValues } = useForm();
+
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+        const { email, password } = getValues() as SignInType;
+
+        // Add another validations
+        if (email && password) {
+            signUpNewUser(getValues())
+                .then(response => {
+                    // show toast to check the email instead of navigate
+                    // if (response.data.user?.aud) navigate('/');
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+    React.useEffect(() => {
+        setValues({ email: '', password: '' });
+
+        return () => {
+            setValues({});
+        };
+    }, []);
+
+    const { email, password } = formData;
+
+    return (
+        formData && (
+            <section className="bg-gray-200 flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
+                <div className="flex flex-col justify-center px-6 py-12 lg:px-8 rounded-lg bg-white shadow-md xl:w-3/12 md:w-6/12 sm:w-10/12">
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    />
+                    <h2 className="text-center text-lg">Create account</h2>
+
+                    <form className="mt-6 grid grid-cols-4 gap-4 grid-flow-row-dense" onSubmit={handleSubmit}>
+                        <div className="col-span-4">
+                            <label className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                            <div className="mt-2.5">
+                                <input
+                                    className="col-span-4 w-full px-2 py-2 resize-none  block rounded-md  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600  outline-none"
+                                    placeholder="your-email@gmail.com"
+                                    name="email"
+                                    onChange={handleInputChange}
+                                    value={email || ''}
+                                    autoFocus
+                                />
+                            </div>
+                        </div>
+                        <div className="col-span-4">
+                            <label className="block text-sm font-semibold leading-6 text-gray-900">Password</label>
+                            <div className="mt-2.5">
+                                <input
+                                    type="password"
+                                    className="col-span-4 w-full px-2 py-2 resize-none  block rounded-md  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600  outline-none"
+                                    name="password"
+                                    onChange={handleInputChange}
+                                    value={password || ''}
+                                    placeholder="*********"
+                                />
+                            </div>
+                        </div>
+                        <div className="col-span-4">
+                            <button className="w-full mt-2 px-4 py-1.5 justify-center rounded-md bg-indigo-600 text-sm leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-medium">
+                                Create
+                            </button>
+                        </div>
+                    </form>
+                    <p
+                        className="text-center overflow-hidden before:h-[1px] after:h-[1px] after:bg-gray-300 
+           after:inline-block after:relative after:align-middle after:w-1/4 
+           before:bg-gray-300 before:inline-block before:relative before:align-middle 
+           before:w-1/4 before:right-2 after:left-2 text-xs text-gray-600 py-4 font-medium"
+                    >
+                        Or continue with
+                    </p>
+
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <button
+                            onClick={signInWithGoogle}
+                            type="button"
+                            className="w-full flex items-center px-3 py-1.5 justify-center rounded-md bg-gray-50 text-sm leading-6 text-gray-600 shadow-sm hover:bg-gray-100 transition-all duration-150 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 border-solid ring-slate-300 ring-1"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="15.25" viewBox="0 0 488 512">
+                                <path
+                                    fill="currentColor"
+                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                                />
+                            </svg>
+                            <span className="text-gray-600 ml-2 font-medium">Sign in with google</span>
+                        </button>
+                    </div>
+                    <p className="mt-6 text-xs text-center text-gray-500">
+                        Already a user?
+                        <Link to="/login" className="ml-1 font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Login
+                        </Link>
+                    </p>
+                </div>
+            </section>
+        )
+    );
+};
+
+export default RegisterPage;
