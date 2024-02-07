6eb88ea16f81b28007455fd3feef7a0bf1cd853b
gonchesan <gmsanchezgaray@gmail.com>
2024-02-07 19:36:34 -0300

    Add (Button):logic, props & implement classNames

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,42 +1,73 @@
 import React from 'react';
+import classnames from 'classnames';
 
-type ButtonShape = ['default', 'circle', 'round'];
+import Spinner from '@/assets/icons/spinner.svg?react';
 
-type ButtonProps = {
-    // type?: ButtonType; // primary | dashed | link | text | default
+type ButtonShape = 'default' | 'circle' | 'round';
+type ButtonType = 'primary' | 'secondary' | 'link' | 'text' | 'default';
+type ButtonSize = 'large' | 'middle' | 'small';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     icon?: React.ReactNode;
     shape?: ButtonShape;
-    // size?: SizeType; large | middle | small
-    disabled?: boolean;
-    loading?: boolean | { delay?: number };
-    className?: string;
-    rootClassName?: string;
-    ghost?: boolean;
-    danger?: boolean;
     block?: boolean;
+    appearance?: ButtonType;
+    loading?: boolean;
+    className?: string;
     children?: React.ReactNode;
-    [key: `data-${string}`]: string;
-    classNames?: { icon: string };
-    styles?: { icon: React.CSSProperties };
-};
+    size?: ButtonSize;
+}
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({
+    children,
+    block,
+    shape = 'default',
+    onClick,
+    className,
+    disabled,
+    icon,
+    appearance = 'default',
+    loading,
+    size = 'middle',
+    ...props
+}) => {
     function handleClick(event: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) {
-        //@ts-ignore
-        const { onClick } = props;
-        //@ts-ignore
-        if (innerLoading || mergedDisabled) {
-            event.preventDefault();
-            return;
-        }
+        event.preventDefault();
         (onClick as React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>)?.(event);
     }
 
     return (
         <button
-            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            {...props}
+            className={classnames(
+                'inline-flex justify-center items-center gap-x-1.5 font-semibold ring-1 ring-inset transition duration-200',
+                { 'w-full': block },
+                //@ shapes
+                { 'rounded-none': shape === 'default' },
+                { 'rounded-md': shape === 'round' },
+                { 'rounded-full': shape === 'circle' },
+                //@ className by hand
+                [className].join(' '),
+                //@ disabled
+                { 'disabled:bg-gray-300': disabled || loading },
+                { 'disabled:text-gray-400': disabled || loading },
+                { 'disabled:cursor-not-allowed': disabled || loading },
+                //@ appearance
+                { 'bg-white text-gray-900 ring-gray-300 hover:bg-gray-50 shadow-sm': appearance === 'default' },
+                { 'bg-teal-600 text-gray-100 ring-gray-300 hover:bg-teal-500 shadow-sm': appearance === 'primary' },
+                { 'bg-gray-900 text-gray-100 ring-transparent hover:bg-gray-800 shadow-sm': appearance === 'secondary' },
+                { 'bg-transparent text-teal-600 ring-transparent hover:text-teal-500': appearance === 'link' },
+                { 'bg-transparent text-gray-900 ring-transparent': appearance === 'text' },
+                //@ sizes
+                { 'px-2 py-1.5 text-xs': size === 'small' },
+                { 'px-3 py-2 text-sm': size === 'middle' },
+                { 'px-4 py-2 text-lg': size === 'large' }
+            )}
+            disabled={disabled || loading}
             onClick={handleClick}
         >
+            {loading ? <Spinner /> : null}
+            {icon && !loading ? icon : null}
             {children}
         </button>
     );
