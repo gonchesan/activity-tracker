acd641588953be8b7cfd5afe67eb9350002f5096
gonchesan <gmsanchezgaray@gmail.com>
2024-02-20 16:22:26 -0300

    Change (Auth):interface

--- src/interface/auth.ts
+++ src/interface/auth.ts
@@ -1,41 +1,41 @@
 import { AuthError, Session, User, UserMetadata, WeakPassword } from '@supabase/supabase-js';
 
-export type AuthProviderProps = {
-    signUpNewUser(params: SignInType): Promise<{
-        data:
-            | {
-                  user: User | null;
-                  session: Session | null;
-              }
-            | {
-                  user: null;
-                  session: null;
-              };
-        error: AuthError | null;
-    }>;
-    signInWithEmail(params: SignInType): Promise<{
-        data:
-            | {
-                  user: User;
-                  session: Session;
-                  weakPassword?: WeakPassword | undefined;
-              }
-            | {
-                  user: null;
-                  session: null;
-                  weakPassword?: null | undefined;
-              };
-        error: AuthError | null;
-    }>;
-    signInWithGoogle: () => Promise<
-        | {
-              provider: string;
-              url: string;
-          }
-        | undefined
-    >;
-    signOut: () => Promise<void>;
-    user: UserMetadata;
+export type AuthContextProps = {
+  signUpNewUser(params: SignInType): Promise<{
+    data:
+      | {
+          user: User | null;
+          session: Session | null;
+        }
+      | {
+          user: null;
+          session: null;
+        };
+    error: AuthError | null;
+  }>;
+  signInWithEmail(params: SignInType): Promise<{
+    data:
+      | {
+          user: User;
+          session: Session;
+          weakPassword?: WeakPassword | undefined;
+        }
+      | {
+          user: null;
+          session: null;
+          weakPassword?: null | undefined;
+        };
+    error: AuthError | null;
+  }>;
+  signInWithGoogle: () => Promise<
+    | {
+        provider: string;
+        url: string;
+      }
+    | undefined
+  >;
+  signOut: () => Promise<void>;
+  user: UserMetadata;
 };
 
 export type SignInType = { email: string; password: string };
