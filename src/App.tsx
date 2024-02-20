46f3303421f671b1e3a7ac8c656cfe5ce74c6e7c
gonchesan <gmsanchezgaray@gmail.com>
2024-02-20 16:27:03 -0300

    Add (App):DatePickerProvider

--- src/App.tsx
+++ src/App.tsx
@@ -10,43 +10,46 @@ import ActivitiesPage from '@/pages/Activities';
 import LoginPage from '@/pages/Login';
 import ErrorPage from '@/pages/Error';
 import RegisterPage from '@/pages/Register';
+import { DatePickerProvider } from './context/DatePickerContext';
 
 function App() {
-    return (
-        <AuthContextProvider>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route
-                        path="/"
-                        element={
-                            <ActivityProvider>
-                                <FormProvider>
-                                    <ActivitiesPage />
-                                </FormProvider>
-                            </ActivityProvider>
-                        }
-                    />
-                </Route>
-                <Route
-                    path="/login"
-                    element={
-                        <FormProvider>
-                            <LoginPage />
-                        </FormProvider>
-                    }
-                />
-                <Route
-                    path="/register"
-                    element={
-                        <FormProvider>
-                            <RegisterPage />
-                        </FormProvider>
-                    }
-                />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </AuthContextProvider>
-    );
+  return (
+    <AuthContextProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <DatePickerProvider>
+                <ActivityProvider>
+                  <FormProvider>
+                    <ActivitiesPage />
+                  </FormProvider>
+                </ActivityProvider>
+              </DatePickerProvider>
+            }
+          />
+        </Route>
+        <Route
+          path="/login"
+          element={
+            <FormProvider>
+              <LoginPage />
+            </FormProvider>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <FormProvider>
+              <RegisterPage />
+            </FormProvider>
+          }
+        />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
