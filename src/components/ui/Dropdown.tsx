4e4f25af55a17980ccbdd0feea7b570425910b38
gonchesan <gmsanchezgaray@gmail.com>
2024-03-19 23:30:26 -0300

    Change Dropdown className

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -73,7 +73,7 @@ const Dropdown: React.FC<DropdownType> = ({ button, menu }) => {
 
       <dialog
         ref={dialogRef}
-        className="absolute left-[-9rem] z-10 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        className="absolute left-[-9rem] z-40 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         role="menu"
         aria-orientation="vertical"
         aria-labelledby="menu-button"
