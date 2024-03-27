3874d901073456b4542205cb7c75fc94d3f1ef25
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2024-03-27 14:45:36 -0300

    Remove buttons to shift the date

--- src/components/ui/DatePicker.tsx
+++ src/components/ui/DatePicker.tsx
@@ -3,9 +3,6 @@ import './css/react-datepicker.css';
 
 import { getAbbreviationDay, pad2Number } from '@/services/date';
 
-import ChevronLeftIcon from '@/assets/icons/chevrons-left.svg?react';
-import ChevronRigthIcon from '@/assets/icons/chevrons-right.svg?react';
-
 import useDatePicker from '@/hooks/useDatePicker';
 
 import Button from './Button';
@@ -18,13 +15,8 @@ type DatePickerType = {
 const DatePicker: React.FC<DatePickerType> = ({ openCreateModal }) => {
   const { setCurrentDate, currentDate, surroundingDates } = useDatePicker();
 
-  function shiftCurrentDate(direction: 'forward' | 'backward') {
-    const daysToAdd = direction === 'forward' ? 1 : -1;
-    setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + daysToAdd)));
-  }
-
   return (
-    <div className="sticky top-0 z-30 bg-white  rounded-xl ring-1 ring-slate-300 shadow-sm">
+    <div className="sticky top-0 z-10 bg-white  rounded-xl ring-1 ring-slate-300 shadow-sm">
       <aside className="flex items-start justify-between border-b border-b-gray-200 border-solid px-4 pt-3 pb-1">
         <div className="flex flex-col items-start">
           <Calendar currentDate={currentDate} setCurrentDate={setCurrentDate} />
@@ -34,22 +26,15 @@ const DatePicker: React.FC<DatePickerType> = ({ openCreateModal }) => {
         </Button>
       </aside>
       <div className="flex items-center justify-around">
-        <Button
-          onClick={() => shiftCurrentDate('backward')}
-          shape="round"
-          appearance="link"
-          icon={<ChevronLeftIcon />}
-        />
-
         {surroundingDates
           ? surroundingDates.map(date =>
               currentDate?.getDate() === date.getDate() ? (
                 <div
                   key={pad2Number(date.getTime())}
-                  className="inline-flex items-center gap-x-1.5 px-3 py-2 transition duration-200 flex-col"
+                  className="inline-flex items-center gap-x-1.5 px-1.5 py-3 transition duration-200 flex-col bg-gray-900 rounded-3xl"
                 >
-                  <span className="text-gray-900 font-medium text-xs">{getAbbreviationDay(date.getDay())}</span>
-                  <span className="w-8 h-8 grid place-content-center rounded-full font-medium bg-teal-600 text-gray-100 ring-gray-300">
+                  <span className="text-gray-100 font-medium text-xs">{getAbbreviationDay(date.getDay())}</span>
+                  <span className="relative w-8 h-8 grid place-content-center rounded-full font-medium  text-gray-100 ring-gray-300 before:absolute before:bg-gray-100 before:-bottom-1 before:left-1/2 before:w-1 before:h-1 before:rounded-full before:-translate-x-1/2">
                     {date.getDate()}
                   </span>
                 </div>
@@ -61,19 +46,11 @@ const DatePicker: React.FC<DatePickerType> = ({ openCreateModal }) => {
                   onClick={() => setCurrentDate(date)}
                 >
                   <span className="text-gray-400 font-medium text-xs">{getAbbreviationDay(date.getDay())}</span>
-                  <span className="w-8 h-8 bg-slate-300 grid place-content-center rounded-full font-medium">
-                    {date.getDate()}
-                  </span>
+                  <span className="w-8 h-8  grid place-content-center rounded-full font-medium">{date.getDate()}</span>
                 </Button>
               )
             )
           : null}
-        <Button
-          onClick={() => shiftCurrentDate('forward')}
-          shape="round"
-          appearance="link"
-          icon={<ChevronRigthIcon />}
-        />
       </div>
     </div>
   );
