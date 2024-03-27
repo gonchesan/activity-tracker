4204adaf31343f0d827c085c030b33950f113a65
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2024-03-27 14:48:56 -0300

    Change z index

--- src/components/activities/TimeLine.tsx
+++ src/components/activities/TimeLine.tsx
@@ -11,7 +11,7 @@ const TimeLine: React.FC<TimeLineType> = ({ bulletPoints }) => {
     <ul className="w-full pt-3 pl-8 relative before:h-full before:border-solid before:border before:border-gray-300 before:absolute before:left-4">
       {bulletPoints.map(activity => (
         <li key={activity.id} className="relative">
-          <span className="w-8 h-8 bg-gray-200 before:w-2/4 before:h-2/4 before:rounded-full before:absolute before:ring-2 before:ring-inset before:ring-gray-400 absolute z-10 rounded-full left-[-32px] before:left-2 before:top-2 top-0"></span>
+          <span className="w-8 h-8 bg-gray-200 before:w-2/4 before:h-2/4 before:rounded-full before:absolute before:ring-2 before:ring-inset before:ring-gray-400 absolute z-5 rounded-full left-[-32px] before:left-2 before:top-2 top-0"></span>
           <ActivityCard activity={activity} />
         </li>
       ))}
