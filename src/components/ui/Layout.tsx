025fc09ff38816a7a1e24ac67bce4ea7fbf44489
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2024-03-27 14:48:29 -0300

    Add Navigate to handle the routing

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import Navbar from '@/components/ui/Navbar';
 import Topbar from '@/components/ui/Topbar';
+import useAuth from '@/hooks/useAuth';
 
 const Layout: React.FC = () => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
   return (
-    <>
-      <div className="mx-auto w-screen bg-white overflow-y-hidden h-screen">
-        <Topbar />
-        <main className="h-[calc(100vh-9.25rem)]">
-          <Outlet />
-        </main>
-        <Navbar />
-      </div>
-    </>
+    <div className="mx-auto w-screen bg-white overflow-y-hidden h-screen">
+      <Topbar />
+      <main className="h-[calc(100vh-9.25rem)]">
+        <Outlet />
+      </main>
+      <Navbar />
+    </div>
   );
 };
 
