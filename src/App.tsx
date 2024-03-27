ce8f2fe209d87e392746aaca563b07baedc7f352
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2024-03-27 14:51:46 -0300

    Add routes and PublicPages to handle the routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,66 @@ import { Route, Routes } from 'react-router-dom';
 import { AuthContextProvider } from '@/contexts/AuthContext';
 import { ActivityProvider } from '@/contexts/ActivityContext';
 import { FormProvider } from '@/contexts/FormContext';
+import { DatePickerProvider } from '@/contexts/DatePickerContext';
 
 import Layout from '@/components/ui/Layout';
+import PublicPages from '@/components/ui/PublicPages';
 
 import ActivitiesPage from '@/pages/Activities';
 import LoginPage from '@/pages/Login';
 import ErrorPage from '@/pages/Error';
 import RegisterPage from '@/pages/Register';
-import { DatePickerProvider } from './contexts/DatePickerContext';
 
 function App() {
   return (
     <AuthContextProvider>
       <Routes>
+        <Route element={<PublicPages />}>
+          <Route
+            path="/login"
+            element={
+              <FormProvider>
+                <LoginPage />
+              </FormProvider>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <FormProvider>
+                <RegisterPage />
+              </FormProvider>
+            }
+          />
+        </Route>
+
         <Route element={<Layout />}>
           <Route
-            path="/"
+            path="/home"
+            element={
+              <DatePickerProvider>
+                <ActivityProvider>
+                  <FormProvider>
+                    <ActivitiesPage />
+                  </FormProvider>
+                </ActivityProvider>
+              </DatePickerProvider>
+            }
+          />
+          <Route
+            path="/activities"
+            element={
+              <DatePickerProvider>
+                <ActivityProvider>
+                  <FormProvider>
+                    <ActivitiesPage />
+                  </FormProvider>
+                </ActivityProvider>
+              </DatePickerProvider>
+            }
+          />
+          <Route
+            path="/statistic"
             element={
               <DatePickerProvider>
                 <ActivityProvider>
@@ -30,22 +74,6 @@ function App() {
             }
           />
         </Route>
-        <Route
-          path="/login"
-          element={
-            <FormProvider>
-              <LoginPage />
-            </FormProvider>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <FormProvider>
-              <RegisterPage />
-            </FormProvider>
-          }
-        />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </AuthContextProvider>
