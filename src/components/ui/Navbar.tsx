ccad9db7abf2d409f28f220208ce826eda22fcbc
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2024-03-27 14:49:52 -0300

    Add NAV_ROUTES & NavLink with his respective Icons

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classnames from 'classnames';
+
 import useAuth from '@/hooks/useAuth';
+
+import HomeIcon from '@/assets/icons/home.svg?react';
+import ActivitiesIcon from '@/assets/icons/clock-filled.svg?react';
+import StatisticIcon from '@/assets/icons/statistic.svg?react';
+
+const NAV_ROUTES = [
+  { title: 'Home', slug: '/home', icon: <HomeIcon /> },
+  { title: 'Activities', slug: '/activities', icon: <ActivitiesIcon /> },
+  { title: 'Statistic', slug: '/statistic', icon: <StatisticIcon /> },
+];
 
 const Navbar: React.FC = () => {
   const { user } = useAuth();
@@ -20,23 +33,26 @@ const Navbar: React.FC = () => {
 
   return (
     user && (
-      <nav className="fixed bg-sky-950 w-[calc(100%-1rem)] bottom-0 rounded-lg m-2 z-10 flex">
-        <button className="p-3 m-3 text-teal-600">
-          <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-            <path
-              fill="currentColor"
-              d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
-            />
-          </svg>
-        </button>
-        <button className="p-3 m-3 text-teal-600">
-          <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-            <path
-              fill="currentColor"
-              d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
-            />
-          </svg>
-        </button>
+      <nav className="fixed bg-gray-100 w-[calc(100%-1rem)] bottom-0 rounded-lg m-2 z-5 flex">
+        {NAV_ROUTES.map(route => (
+          <NavLink
+            key={route.title}
+            title={route.title}
+            to={route.slug}
+            className={({ isActive }) =>
+              classnames(
+                'relative p-3 m-3 transition duration-200 before:absolute  before:bottom-0 before:left-1/2 before:w-1 before:h-1 before:rounded-full before:-translate-x-1/2',
+                {
+                  'text-gray-900 before:absolute before:bg-gray-900 before:bottom-0 before:left-1/2 before:w-1 before:h-1 before:rounded-full before:-translate-x-1/2':
+                    isActive,
+                  'text-gray-400 before:bg-transparent': !isActive,
+                }
+              )
+            }
+          >
+            {route.icon}
+          </NavLink>
+        ))}
       </nav>
     )
   );
